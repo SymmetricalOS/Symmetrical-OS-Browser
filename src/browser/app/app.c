f1f93d266cf720f8569e948c3e3e085b6c989abe
pizcan <140350299+pisscan@users.noreply.github.com>
2023-10-11 18:58:38 -0700

    Update app.c

--- src/browser/app/app.c
+++ src/browser/app/app.c
@@ -2,4 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
-struct 
+struct init {
+  static void wn_init() {
+    RN_INIT
+  }
+}
+
+int main(int argc, char* argv[]) {
+  init run;
+  run.wn_init();
+}
